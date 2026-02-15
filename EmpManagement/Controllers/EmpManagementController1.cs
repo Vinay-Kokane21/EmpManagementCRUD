e531c5701b094eefba09fc3d85609b9b94f87296
Vinay Kokane21 <kokanevinay55@gmail.com>
2026-02-15 13:42:47 +0530

    Controller Crud code Completed

--- EmpManagement/Controllers/EmpManagementController1.cs
+++ EmpManagement/Controllers/EmpManagementController1.cs
@@ -1,4 +1,5 @@
 ﻿using EmpManagement.AppDb;
+using EmpManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmpManagement.Controllers
@@ -11,9 +12,85 @@ namespace EmpManagement.Controllers
             _context = context;
             
         }
+        //Read
         public IActionResult Index()
         {
+            List<Employee> empentry = _context.Employees.ToList();
+            return View(empentry);
+        }
+
+        //Create
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Employees.Add(emp);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        //Update
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id == null && id == 0)
+            {
+                return NotFound("Not Found");
+            }
+
+            Employee? emp = _context.Employees.Find(id);
+            if(emp == null)
+            {
+                return NotFound();
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Employees.Update(emp);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+
+        //Delete
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null && id == 0)
+            {
+                return NotFound("Not Found");
+            }
+
+            Employee? emp = _context.Employees.Find(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Employee emp)
+        {
+            _context.Employees.Remove(emp);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+                     
         }
     }
 }
