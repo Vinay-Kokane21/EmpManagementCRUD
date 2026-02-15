9d61ce090e344b66b85b5af41c4528b3bc469229
Vinay Kokane21 <kokanevinay55@gmail.com>
2026-02-15 13:09:42 +0530

    Add project files.

---
+++ EmpManagement/AppDb/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmpManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpManagement.AppDb
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmpManagement/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmpManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Role { get; set; }
+    }
+}
