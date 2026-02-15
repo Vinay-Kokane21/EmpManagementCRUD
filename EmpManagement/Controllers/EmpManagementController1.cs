ec741c60f41317ddb537a4b60d74995812fc98a4
Vinay Kokane21 <kokanevinay55@gmail.com>
2026-02-15 13:14:28 +0530

    Migration and New Controller added

---
+++ EmpManagement/Controllers/EmpManagementController1.cs
@@ -0,0 +1,19 @@
+﻿using EmpManagement.AppDb;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpManagement.Controllers
+{
+    public class EmpManagementController1 : Controller
+    {
+        private readonly AppDbContext _context;
+        public EmpManagementController1(AppDbContext context)
+        {
+            _context = context;
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
